9427d3a602fd8903f72ce46613014434ae48f97f
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-30 15:13:36 -0300

    Criando formulario e salvando dados no banco de dados

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,30 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-<h1>Criar nono evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie seu evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" name="title" id="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" name="city" id="city" placeholder="Local do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">O evento é privado</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Descrição</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar Evento">
+    </form>
+</div>
 @endsection
