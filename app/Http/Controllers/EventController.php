c1dbb9057de0b124d83978a1784aa252ec0e233d
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-02 16:18:27 -0300

    Salvando JSON no banco de dados

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -25,7 +25,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
-
+        $event->items = $request->items;
 
         //Upload de imagem
         if($request->hasFile('image') && $request->file('image')->isValid()){
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -7,7 +7,7 @@
 <div class="col-md-10 offset-md-1">
     <div class="row">
         <div id="image-container" class="col-md-6">
-            <img src="/img/event_placeholder.jpg" class="img-fluid" alt="{{ $event->title }}">
+            <img src="/img/events/{{ $event->image }}" class="img-fluid" alt="{{ $event->title }}">
         </div>
         <div id="info-container" class="col-md-6">
             <h1>{{ $event->title }}</h1>
@@ -15,6 +15,12 @@
             <p class="event-participants"><ion-icon name="people-outline"></ion-icon>X participantes</p>
             <p class="event-owner"><ion-icon name="star-outline"></ion-icon>Dono do Evento</p>
             <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+            <h3>O evento conta com:</h3>
+            <ul id="items-list">
+                @foreach($event->items as $item)
+                    <li><ion-icon name="play-outline"></ion-icon><span>{{ $item }}</span></li>
+                @endforeach
+            </ul>
         </div>
         <div class="col-md-12" id="description-container">
             <h3>Sobre o evento</h3>
