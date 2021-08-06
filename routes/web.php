9712c01204c59e11e76d65c88aac47743c9fce5e
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-06 15:03:20 -0300

    Autenticação no Laravel (login/registro)

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('/contact', function(){
 
     return view('contact');
 });
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
