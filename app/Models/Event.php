38b63e6b63cea1c2716d9fc87157957ee21d0cdf
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-10 14:17:20 -0300

    Update no laravel

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -12,6 +12,8 @@ class Event extends Model
 
     protected $dates = ['date'];
 
+    protected $guarded = [];
+
     public function user(){
         return $this->belongsTo('App\Models\User');
     }
